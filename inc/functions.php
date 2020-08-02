b4c8992a1a7684c6172c48f53ed503ff2ec02554
Devis <22038252+DevisWeb@users.noreply.github.com>
2020-08-02 17:15:18 +0200

    Update function printQuote, clean-up comments
    Change used function for function printQuote: using rand() as required  instead of array_rand()

--- inc/functions.php
+++ inc/functions.php
@@ -1,37 +1,17 @@
 <?php
 // PHP - Random Quote Generator
 
-// Create the Multidimensional array of quote elements and name it quotes
-// Each inner array element should be an associative array
-
-/* 
- * quote - string: text of quote displayed on page
- * source - string: containing creator of quote ("Mark Twain" or "Traditional Irish proverb” or "Anonymous")
- * citation - optional - string identifying where quote comes from, like a speech, publication or a movie. If not known => NOT include item in inner array
- * year - optional - number identifying year of the quote. If not known => NOT include item in inner array
-   ==> create printQuote() function that prints the quotes to the screen.
-
-// create at least 5 quotes with at least two items!
-
-$quotes = array(
-    array(
-        "quote" => "",      // *required
-        "source" => "",     // *required
-        "citation" => "",   //  optional
-        "year" => ,         //  optional
-        ),
-        (
-        ...
-        )
-)
-*/
+// #################################################################
+// Create multidimensional array $quotes (takes one param: array)
+// with at least 5 quotes (associative inner array) 
+// with at least two default items (quote, source) + two optional (citation, year)
 
 $quotes = array(
     [
         "quote" => "quote1",
         "source" => "src1",
         "citation" => "cit1", 
-        "year" => "2020",
+        "year" => 2020,
     ],
     ["quote" => "quote2",
         "source" => "src2"
@@ -45,100 +25,66 @@ $quotes = array(
     ["quote" => "quote5",
         "source" => "src5"
     ]
-);
-echo "<pre>";
-// print_r($quotes);
-echo "</pre>";
+); 
 
 // #################################################################
-// Create the getRandomQuuote function and name it getRandomQuote
-// function takes one param called array ==> argument passed is array of quotes
-
-// body of function:
-// selects+returns random quote element from quotes array
-// ==> remember method of finding random number_format
+// Create getRandomQuote function (one param: array)
+// select + return random quote element from quotes array
 // select items with "box notation" array[0]
 
- function getRandomQuote(array $quotes) { 
-    // generate a random number between 0 and the last index in the array parameter
-    // use the random number and box notation to grab a random item from the array
-    // return the random item
-    
-  $randomQuote = array_rand($quotes);
-     return $quotes[$randomQuote];
-
-// test output all:
-  /*foreach ($quotes[$randomQuote] as $key => $val)
-  {
-  // echo $quotes[$randomQuote]["quote"];
-  echo $val . "<br>";
-  } */
+// ### using required function - rand — "Generate a random integer":
+// https://www.php.net/manual/en/function.rand.php
+function getRandomQuote($arr) {  
+    $randomInt = rand(0, count($arr) - 1);
+    return $arr[$randomInt];
 } 
 
-// check output:
- // $quoteItem = getRandomQuote($quotes);
- // echo $quoteItem["quote"] . "<br>";
-
-// ### Variant 2:
-/* function getRandomQuote(array $arr){
-    $randomKey = rand(0, count($arr) - 1);
-    echo $arr[$randomKey];
+// ### Variant 2 - array_rand — "Pick one or more random keys out of an array:
+// (https://www.php.net/manual/en/function.array-rand.php)
+/* function getRandomQuote2($arr){
+    $randomQuote = array_rand($arr);
+     return $arr[$randomQuote];
     } 
+** Compare Outputs - rand vs array_rand:
+    $randoutput = getRandomQuote($quotes);
+        print_r($randoutput); // Array ( [quote] => quote5 [source] => src5 ) 
+        echo "<br/>"; 
+    $randoutput2 = getRandomQuote2($quotes);
+        print_r($randoutput2); // Array ( [quote] => quote5 [source] => src5 ) 
 */
 
 // #################################################################
-// printQuote funtion takes one param called array 
-
-// body of function:
-// getRandomQuote should be called on array of quote obejcts, 
-// and the result stored in a variable.
-// new empty string should be created and stored in a variable.
-// required quote array items + optional quote items if exist, 
-// should be appropriately concatenated to the new string 
-// created using the following HTML template:
-/*
+// printQuote function (one param called array) 
+/* (**) HTML template :
         <p class="quote"> [quote here] </p>
         <p class="source"> [source here]
         <span class="citation"> [citation here] </span>
         <span class="year"> [year here] </span>
         </p> 
 */
-// span elements that hold citation+year properties 
-// should only be created 
-// if current quote element has those items. 
-// This is where your conditionals will come in handy.
-// The printQuote function should display the completed HTML string
-
-function  printQuote(array $quotes) {
-   // create a variable that calls the getRandomQuote() function
-   // passing in the quotes array as an argument
-    $quoteItem = getRandomQuote($quotes); 
-   
-   // create a variable that initiates your HTML string
+function  printQuote($arr) {
+   // create variable calling getRandomQuote() function
+   // with quotes array as argument
+    $quoteItem = getRandomQuote($arr); 
+   // create variable to initiate HTML string:
     $quoteOutput = "";
-   // using the template in the project instructions, 
+   // use template in the project instructions (** see above),
+   // add the two default quote properties
     $quoteOutput .= '<p class="quote">' . $quoteItem["quote"] . '</p>';
     $quoteOutput .= '<p class="source">' . $quoteItem["source"];
-
-   // add the two default quote properties
-   // if the quote contains a citation value, add it the string
-   // if the quote contains a year value, add it the string
-
+   // if quote contains citation value, add to string
+   // if quote contains year value, add to string
     if( isset($quoteItem['citation'])){
         $quoteOutput .= '<span class="citation">' . $quoteItem["citation"] . '</span>';
     }
     if( isset($quoteItem['citation'])){
-        $quoteOutput .= '<span class="year">' . $quoteItem["citation"] . '</span>';
+        $quoteOutput .= '<span class="year">' . $quoteItem["year"] . '</span>';
     }
-   // close the string with the necessary closing HTML tags
-   
-    $quoteOutput .= '</p>';  
+   // close string with the necessary closing HTML tags
+   $quoteOutput .= '</p>';  
    
 return $quoteOutput;
 }
-printQuote($quotes);
+// printQuote($quotes);
 
 // ##################################################################
-
-
-
