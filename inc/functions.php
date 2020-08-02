583da9d0dd0db3fc8ffe2a90e41e656257679556
Devis <22038252+DevisWeb@users.noreply.github.com>
2020-08-02 18:50:24 +0200

    Add Instructions for ExtraCredit (3):  Complete Task 1 - Add Item to Array
    Add Item to String, Add class Styles
    --> Task 1 of 3 complete

--- inc/functions.php
+++ inc/functions.php
@@ -5,26 +5,38 @@
 // Create multidimensional array $quotes (takes one param: array)
 // with at least 5 quotes (associative inner array) 
 // with at least two default items (quote, source) + two optional (citation, year)
+// for "exceeds" rating: add more items to inner quote (category)
+$cat = array("business", "humor", "motivational", "technology", "zen");
 
 $quotes = array(
     [
-        "quote" => "quote1",
-        "source" => "src1",
-        "citation" => "cit1", 
-        "year" => 2020,
+    "quote" => "quote1",
+    "source" => "src1",
+    "citation" => "cit1", 
+    "year" => 2020,
+    "cat" => $cat[1]
     ],
-    ["quote" => "quote2",
-        "source" => "src2"
+    [   
+    "quote" => "quote2",
+    "source" => "src2",
+    "cat" => $cat[1]
     ],
-    ["quote" => "quote3",
-        "source" => "src3"
+    [   
+    "quote" => "quote3",
+    "source" => "src3",
+    "cat" => $cat[1]
     ],
-    ["quote" => "quote4",
-        "source" => "src4"
+    [   
+    "quote" => "quote4",
+    "source" => "src4",
+    "cat" => $cat[1]
     ],
-    ["quote" => "quote5",
-        "source" => "src5"
-    ]
+    [   
+    "quote" => "quote5",
+    "source" => "src5",
+    "citation" => "cit5", 
+    "year" => 2015,
+    ],
 ); 
 
 // #################################################################
@@ -64,27 +76,45 @@ function getRandomQuote($arr) {
 */
 function  printQuote($arr) {
    // create variable calling getRandomQuote() function
-   // with quotes array as argument
+   // with quotes array as argument:
     $quoteItem = getRandomQuote($arr); 
    // create variable to initiate HTML string:
     $quoteOutput = "";
    // use template in the project instructions (** see above),
-   // add the two default quote properties
+   // add the two default quote properties:
     $quoteOutput .= '<p class="quote">' . $quoteItem["quote"] . '</p>';
     $quoteOutput .= '<p class="source">' . $quoteItem["source"];
-   // if quote contains citation value, add to string
-   // if quote contains year value, add to string
+   // if quote contains citation value, add to string,
+   // if quote contains year value, add to string:
     if( isset($quoteItem['citation'])){
         $quoteOutput .= '<span class="citation">' . $quoteItem["citation"] . '</span>';
     }
     if( isset($quoteItem['citation'])){
         $quoteOutput .= '<span class="year">' . $quoteItem["year"] . '</span>';
     }
-   // close string with the necessary closing HTML tags
-   $quoteOutput .= '</p>';  
+   // for "exceeds" rating add extra item to string:
+    if( isset($quoteItem['cat'])){
+        $quoteOutput .= '<span class="cat">' . $quoteItem["cat"] . '</span>';
+    }
+   // close string with the necessary closing HTML tags:
+    $quoteOutput .= '</p>';  
    
-return $quoteOutput;
+    return $quoteOutput;
 }
-// printQuote($quotes);
+// printQuote($quotes); // function is called directly within html quote-box of index.php as required 
 
 // ##################################################################
+// TODO: Extra Credit to get an "exceeds" rating
+
+// 1. Add more items to the inner quote element.
+//    For example, a tags property and add to the string
+// => DONE
+
+// 2. Random background color.
+//    quote change => randomly change background color of page
+// => TODO 
+
+// 3. Auto-refresh the quote.
+//    after a set amount of time (20-30 sec) print new quote to page
+//    (Hint: Meta tags can be one way to accomplish this)
+// => TODO
